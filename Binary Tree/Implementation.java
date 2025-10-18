415cf91d66432b9ef0d7fb765696f11b92707302
anushkas11 <anushkasharma2599@gmail.com>
2025-10-18 17:58:07 +0530

    Binary Tree-Intro/

---
+++ Binary Tree/Implementation.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+     }
+ }
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> nodes = new HashMap<>();
+        Set<Integer> children = new HashSet<>();
+        for (int[] d : descriptions) {
+            nodes.putIfAbsent(d[0], new TreeNode(d[0]));
+            nodes.putIfAbsent(d[1], new TreeNode(d[1]));
+            if (d[2] == 1) nodes.get(d[0]).left = nodes.get(d[1]);
+            else nodes.get(d[0]).right = nodes.get(d[1]);
+            children.add(d[1]);
+        }
+        for (int val : nodes.keySet()) {
+            if (!children.contains(val)) return nodes.get(val);
+        }
+        return null;
+    }
+}
