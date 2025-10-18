4ec813ee6a472088b33625272c88dff186567760
anushkas11 <anushkasharma2599@gmail.com>
2025-10-18 17:38:28 +0530

    Queue-Intro/

---
+++ Queue/QueueUsingStack.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class MyQueue {
+    private Deque<Integer> stack1;
+    private Deque<Integer> stack2;
+
+    public MyQueue() {
+        stack1 = new ArrayDeque<>();
+        stack2 = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        stack1.push(x);
+    }
+    
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+    
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+    
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
