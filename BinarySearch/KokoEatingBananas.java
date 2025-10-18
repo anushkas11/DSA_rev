6594e3765615ac9ff26ca776db7032e679dc9495
anushkas11 <anushkasharma2599@gmail.com>
2025-10-18 17:51:19 +0530

    BinarySearch-1/

---
+++ BinarySearch/KokoEatingBananas.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 0;
+        for (int pile : piles) right = Math.max(right, pile);
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canEatAll(piles, h, mid)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+    
+    private boolean canEatAll(int[] piles, int h, int k) {
+        int hours = 0;
+        for (int pile : piles) hours += (pile + k - 1) / k;
+        return hours <= h;
+    }
+}
