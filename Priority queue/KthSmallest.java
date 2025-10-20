16ca8509288782f9d04c601f2d4f27301ab1a7e1
anushkas11 <anushkasharma2599@gmail.com>
2025-10-20 21:37:58 +0530

    Priority queue-2

---
+++ Priority queue/KthSmallest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int kthSmallest(int[] arr, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : arr) {
+            maxHeap.offer(num);
+            if (maxHeap.size() > k) maxHeap.poll();
+        }
+        return maxHeap.peek();
+    }
+}
